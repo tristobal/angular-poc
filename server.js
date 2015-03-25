a097714f4f26b34aaa260d2b387ce9f1f6387184
Cristobal Sanchez <cristobal.sanchez@coraltis.com>
2015-03-25 17:05:25 -0300

    Agrega grunt-express-server para iniciar el web server como pate de 'grunt watch'

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/build'));
-
-app.listen(process.env.PORT || 3000);
+app.listen(port);
+console.info("Express server listening http://localhost:" + port);
